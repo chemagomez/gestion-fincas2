929b2654c25f869868ca341daf19319275e765c2
Chema <snekman@hotmail.com>
2018-04-17 13:45:47 +0200

    Cambio pantalla principal

--- src/main/java/com/mkyong/controller/DefaultController.java
+++ src/main/java/com/mkyong/controller/DefaultController.java
@@ -6,39 +6,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DefaultController {
 
-    @GetMapping("/")
-    public String home1() {
-        return "/home";
-    }
+	@GetMapping("/")
+	public String home1() {
+		return "/public/index";
+	}
 
-    @GetMapping("/home")
-    public String home() {
-        return "/home";
-    }
+	@GetMapping("/home")
+	public String home() {
+		return "/home";
+	}
 
-    @GetMapping("/admin")
-    public String admin() {
-        return "/admin";
-    }
+	@GetMapping("/admin")
+	public String admin() {
+		return "/admin";
+	}
 
-    @GetMapping("/user")
-    public String user() {
-        return "/user";
-    }
+	@GetMapping("/user")
+	public String user() {
+		return "/user";
+	}
 
-    @GetMapping("/about")
-    public String about() {
-        return "/about";
-    }
+	@GetMapping("/about")
+	public String about() {
+		return "/about";
+	}
 
-    @GetMapping("/login")
-    public String login() {
-        return "/login";
-    }
+	@GetMapping("/login")
+	public String login() {
+		return "/login";
+	}
 
-    @GetMapping("/403")
-    public String error403() {
-        return "/error/403";
-    }
+	@GetMapping("/403")
+	public String error403() {
+		return "/error/403";
+	}
 
 }
