54826ec8b0aa512390d64a419a692d70c47664e1
Chema <snekman@hotmail.com>
2018-04-18 10:13:09 +0200

    espacio

--- src/main/java/com/mkyong/controller/PublicController.java
+++ src/main/java/com/mkyong/controller/PublicController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublicController {
 
-    
-    @GetMapping("/public/index")
-    public String index() {
-        return "/public/index";
-    }    
+	@GetMapping("/public/index")
+	public String index() {
+		return "/public/index";
+	}
 
 }
